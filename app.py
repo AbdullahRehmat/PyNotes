d78f7cefbf2b6db3a81d9871196a7e94ce627402
10CodeDev <10CodeDevelopment@gmail.com>
2020-12-24 10:49:50 +0300

    Inital Commit

---
+++ app.py
@@ -0,0 +1,92 @@
+# Import Modules Here
+import os
+from dotenv import load_dotenv
+from flask_wtf import FlaskForm
+from flask_mde import Mde, MdeField
+from wtforms import SubmitField, StringField
+from wtforms.validators import InputRequired, DataRequired, Length
+from flask import Flask, request, render_template, flash, session, url_for
+
+import time
+import multiprocessing
+import webview
+import os.path
+
+app = Flask(__name__)
+load_dotenv()
+mde = Mde(app)
+app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY')
+
+windowTitle = "PiNotes [MD]"
+fixedPath = '/home/rehmat/Documents/'
+
+
+def error_flasher(form):
+    for field, message_list in form.errors.items():
+        for message in message_list:
+            flash(form[field].label.text + ': ' + message, 'error')
+    print(message, message_list)
+
+
+def fileHandler(form):
+    savePath = os.path.join(fixedPath, form.title.data)
+    f = open(savePath, "w+")
+    f.write(form.editor.data)
+    f.close()
+
+
+def contentLoader(form):
+    savePath = os.path.join(fixedPath, form.title.data)
+    fileName = savePath
+    f = open(fileName, "r")
+    return f.read()
+
+
+def startFlaskServer():
+    app.run()
+
+
+def startWebview():
+    webview.create_window('My Web App', 'http://localhost:5000')
+    webview.start()
+
+
+class MdeForm(FlaskForm):
+
+    title = StringField('Title', validators=[DataRequired()])
+
+    editor = MdeField(
+        validators=[
+            #InputRequired("Input required"),
+            Length(min=0, max=30000)
+        ]
+    )
+    submit = SubmitField()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = MdeForm()
+
+    if form.validate_on_submit() and form.editor.data == "":
+        form.editor.data = contentLoader(form)
+
+    elif form.validate_on_submit() and form.editor.data != "":
+        session['editor'] = request.form['editor']
+        fileHandler(form)
+        form.title.data = ""
+        form.editor.data = ""
+
+    return render_template("index.html", form=form, windowTitle=windowTitle)
+
+
+if __name__ == "__main__":
+
+    p1 = multiprocessing.Process(target=startFlaskServer)
+    p2 = multiprocessing.Process(target=startWebview)
+
+    p1.start()
+    time.sleep(2)
+    p2.start()
+    p2.join()
+    p1.terminate()
